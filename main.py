d979ccbfb881604f0baa97fa136aab7d6a8113fd
aharnish <aharnishpithva@gmail.com>
2023-11-22 17:46:40 +0530

    working only safe exploration with dead_ends

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ legal_directions = ["north","east","south","west"]  # is ordered list, do not ch
     # Wumpus alive
 wumpus_alive = True
 
+    # Gold is still on the board
+gold_on_board = True
+
     # arrows left
 arrows_left = 1
 
@@ -38,7 +41,9 @@ to_visit_stack = []     # stack of coordinates to visit
 dead_ends = []
 
 
-def check_out_of_bounds(x,y):
+def check_out_of_bounds(x,y=None):
+    if(y==None):    # soft overloading fn
+        x,y = x[0],x[1]
     if x<global_grid_xmin or x>global_grid_xmax or y<global_grid_ymin or y>global_grid_ymax:
             return True
     return False
@@ -81,11 +86,11 @@ def get_current_block_info():
 def get_adjacent_blocks_coor(cur_x, cur_y, direction):
     # return the coordinates of the block adjacent to the current block in that direction
     if(direction == "north"):
-        return [cur_x,cur_y+1]
+        return [cur_x,cur_y-1]  # assumming north is y-1
     elif(direction == "east"):
         return [cur_x+1,cur_y]
     elif(direction == "south"):
-        return [cur_x,cur_y-1]
+        return [cur_x,cur_y+1]  # assumming south is y+1
     elif(direction == "west"):
         return [cur_x-1,cur_y]
     else:
@@ -121,13 +126,19 @@ def take_action(action_name):
     """
     Possible actions: forward, left, right, shoot, grab
     """
+    global player_cur_direction
+    global player_cur_loc
+
+    print("Action:",action_name,"   current location:",player_cur_loc,"  current direction:", player_cur_direction)
 
     # read legal directions index
-    cur_direction_index = legal_directions.index(player_cur_direction)
+    cur_direction_index = direction_num(player_cur_direction)
     if(action_name == "left"):
         player_cur_direction = legal_directions[(cur_direction_index-1)%4]
+        print_grid()
     elif(action_name == "right"):
         player_cur_direction = legal_directions[(cur_direction_index+1)%4]
+        print_grid()
     elif(action_name == "forward"):
         get_next_block_coor = get_adjacent_blocks_coor(player_cur_loc[0],player_cur_loc[1],player_cur_direction)
         if(check_out_of_bounds(get_next_block_coor[0],get_next_block_coor[1])):
@@ -172,22 +183,47 @@ def init_model_states():
 def update_knowledge(player_cur_loc):
     x,y= player_cur_loc[0],player_cur_loc[1]
     visited[x][y]=1
-    for i in range(len(get_current_block_info())):
-        if get_current_block_info()[i]!='stench':
-            possible_wumpus = [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]
-            for j in range(len(possible_wumpus)):
-                if (check_out_of_bounds(possible_wumpus[j][0],possible_wumpus[j][1])) and (visited[possible_wumpus[j][0]][possible_wumpus[j][1]]!=1):
-                    wumpus_possible[possible_wumpus[j][0]][possible_wumpus[j][1]]=0
-                else:
-                    continue
 
-        if get_current_block_info()[i]!='breeze':
-            possible_pit = [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]
-            for j in range(len(possible_pit)):
-                if (check_out_of_bounds(possible_pit[j][0],possible_pit[j][1])) and (visited[possible_pit[j][0]][possible_pit[j][1]]!=1):
-                    pit_possible[possible_pit[j][0]][possible_pit[j][1]]=0
-                else:
-                    continue
+    
+
+    current_block_info = get_current_block_info()
+    if not("stench" in current_block_info):
+        possible_wumpus = [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]
+        wumpus_possible[player_cur_loc[0]][player_cur_loc[1]]=0
+        for j in range(len(possible_wumpus)):
+            if (not(check_out_of_bounds(possible_wumpus[j][0],possible_wumpus[j][1]))):
+                wumpus_possible[possible_wumpus[j][0]][possible_wumpus[j][1]]=0
+            else:
+                continue
+
+    if not("breeze" in current_block_info):
+        possible_pit = [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]
+        pit_possible[player_cur_loc[0]][player_cur_loc[1]]=0
+        for j in range(len(possible_pit)):
+            if (not(check_out_of_bounds(possible_pit[j][0],possible_pit[j][1]))):
+                pit_possible[possible_pit[j][0]][possible_pit[j][1]]=0
+            else:
+                continue
+
+
+
+    # # did not mark no wumpus when nothing was returned in get_current_block_info 
+    # for i in range(len(get_current_block_info())):
+    #     if get_current_block_info()[i]!='stench':
+    #         possible_wumpus = [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]
+    #         for j in range(len(possible_wumpus)):
+    #             if (check_out_of_bounds(possible_wumpus[j][0],possible_wumpus[j][1])) and (visited[possible_wumpus[j][0]][possible_wumpus[j][1]]!=1):
+    #                 wumpus_possible[possible_wumpus[j][0]][possible_wumpus[j][1]]=0
+    #             else:
+    #                 continue
+
+    #     if get_current_block_info()[i]!='breeze':
+    #         possible_pit = [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]
+    #         for j in range(len(possible_pit)):
+    #             if (check_out_of_bounds(possible_pit[j][0],possible_pit[j][1])) and (visited[possible_pit[j][0]][possible_pit[j][1]]!=1):
+    #                 pit_possible[possible_pit[j][0]][possible_pit[j][1]]=0
+    #             else:
+    #                 continue
 
 def safe_loc(x,y):
     if (wumpus_possible[x][y]!=1) and (pit_possible[x][y]!=1):
@@ -201,27 +237,55 @@ def planner(cur_x, cur_y):
     for direction in legal_directions:
         # get next direction
         next_visit_block = get_adjacent_blocks_coor(cur_x,cur_y,direction)
-        if (safe_loc(next_visit_block) 
-            and not(visited[next_visit_block[0]][next_visit_block[1]]) 
-            and not(next_visit_block in dead_ends) 
-            and check_out_of_bounds(next_visit_block[0],next_visit_block[1])):
+        if (safe_loc(next_visit_block[0],next_visit_block[1])                                          # the location is safe (no pit, no wumpus)
+            and not(visited[next_visit_block[0]][next_visit_block[1]])          # the location is not visited
+            and not(next_visit_block in dead_ends)                              # the location is not a dead end
+            and not(check_out_of_bounds(next_visit_block[0],next_visit_block[1]))):  # the location is inside the grid
 
-            choices.append(next_visit_block)
+            choices.append(next_visit_block)    # insert to list of possible choices
 
     # if no valid safe option is found in first local search, then it can also visit the last visited node and marks current node as dead_end
-    dead_ends.append(next_visit_block)
-    for direction in legal_directions:
-        if (safe_loc(next_visit_block) and not(next_visit_block in dead_ends) and check_out_of_bounds(next_visit_block[0],next_visit_block[1])):
-            choices.append(next_visit_block)    ## adding last visited node as next to visit node
+    if(len(choices) == 0):
+        dead_ends.append([cur_x,cur_y])
+        for direction in legal_directions:
+            next_visit_block = get_adjacent_blocks_coor(cur_x,cur_y,direction)
+            if (safe_loc(next_visit_block[0],next_visit_block[1]) and not(next_visit_block in dead_ends) and not(check_out_of_bounds(next_visit_block[0],next_visit_block[1]))):
+                choices.append(next_visit_block)    ## adding last visited node as next to visit node
 
-    
+    # After the choice list is generated, pick first and commit first action
+    # add all other choices to to_visit.
+    next_block_final = choices[0]
+
     # if more than one choices, add other choices to_visit_stack
     if(len(choices) > 1):
         for i in range(1,len(choices)):
-            to_visit_stack.append(next_visit_block[i])
-    mark_visited(choices[0][0],choices[0][1])
+            if not(choices[i] in to_visit_stack):
+                to_visit_stack.append(choices[i])
+
+    # mark_visited(next_block_final[0],next_block_final[1])
+    action_implementation(next_block_final)
 
     # for 
+
+def print_grid():
+    # print grid with w for wumpus, p for pit, g for gold, ^v>< for agent with direction
+    for y in range(global_grid_ymin,global_grid_ymax+1):
+        for x in range(global_grid_xmin,global_grid_xmax+1):
+            print(x,",",y,sep="",end="")
+            if([x,y] == player_cur_loc):
+                character_array = ["^",">","v","<"]
+                print(character_array[direction_num(player_cur_direction)],end="")
+            elif([x,y] == wumpus_loc):
+                print("w",end="")
+            elif([x,y] in pit_loc):
+                print("p",end="")
+            elif([x,y] == gold_loc):
+                print("g",end="")
+            else:
+                print(" ",end="")
+            print("\t|",end="")
+        print("\n_________________________________\n")
+
 
 
 
@@ -253,11 +317,11 @@ def direction_num(direction):
 def get_direction_between_blocks(cur_x, cur_y, new_x, new_y):
     # return the direction from the current block to the new block
     if new_x == cur_x and new_y == cur_y + 1:
-        return "north"
+        return "south"      ## changed because assumming y increase in south.
     elif new_x == cur_x + 1 and new_y == cur_y:
         return "east"
     elif new_x == cur_x and new_y == cur_y - 1:
-        return "south"
+        return "north"      ## changed because assumming y increase in south.
     elif new_x == cur_x - 1 and new_y == cur_y:
         return "west"
     else:
@@ -266,42 +330,68 @@ def get_direction_between_blocks(cur_x, cur_y, new_x, new_y):
     
 def action_implementation(new_loc):
     "performs action to go to the new location"
-    x,y= player_cur_loc()[0],player_cur_loc()[1]
+    x,y= player_cur_loc[0],player_cur_loc[1]
     x1,y1=new_loc[0],new_loc[1]
     if check_out_of_bounds(new_loc):
         print("Error, out of bounds")
-    cur_dir= direction_num(player_cur_direction)
+    cur_dir = direction_num(player_cur_direction)
     new_dir = direction_num(get_direction_between_blocks(x,y,x1,y1))
     diff=new_dir-cur_dir
-    if diff ==0 or diff==3:
+    if diff%4==0:
         take_action("forward")
-    if diff ==1:
+    elif diff%4==1:
         take_action("right")
         take_action("forward")
-    if diff ==2 or diff==-2:
+    elif diff ==2 or diff==-2:
         take_action("right")
         take_action("right")
         take_action("forward")
-    if diff ==-1:
+    elif diff ==-1 or diff==3:
         take_action("left")
         take_action("forward")
-    print("the player is at the new location",player_cur_loc) #prints the new location    
+    else:
+        print("Error: Unable to determine direction")
+    if(player_cur_loc != new_loc):
+        print("Error: Unable to reach specified location.")
+    else:
+        mark_visited(player_cur_loc[0],player_cur_loc[1])
+        print("the player is at the new location",player_cur_loc) #prints the new location    
 
 def safe_loc(x,y):
+    if(check_out_of_bounds(x,y)):
+        return False
     if (wumpus_possible[x][y]!=1) and (pit_possible[x][y]!=1):
         return True
     else:
         return False 
 
+def print_knowledge():
+    # print("wumpus_possible",wumpus_possible)
+    # print grid of wumpus_possible
+    print("WUMPUS POSSIBLITIES")
+    for y in range(global_grid_ymin,global_grid_ymax+1):
+        for x in range(global_grid_xmin,global_grid_xmax+1):
+            print(wumpus_possible[x][y],end="|")
+        print("")
+
+    print("PIT POSSIBILITIES")
+    for y in range(global_grid_ymin,global_grid_ymax+1):
+        for x in range(global_grid_xmin,global_grid_xmax+1):
+            print(pit_possible[x][y],end="|")
+        print("")
+    print("VISITED LOCATIONS")
+    for y in range(global_grid_ymin,global_grid_ymax+1):
+        for x in range(global_grid_xmin,global_grid_xmax+1):
+            print(visited[x][y],end="|")
+        print("")
+
 
 init_model_states()
 
-print("wumpus_possible",wumpus_possible)
-print("pit_possible",pit_possible)
-print("visited",visited)
+
 
 ############### Playground Testing ###############
-player_cur_loc = [2,1]
+# player_cur_loc = [2,1]
 print("current block info",get_current_block_info())
 
 
@@ -314,7 +404,12 @@ while(1):
 
     update_knowledge(player_cur_loc)
 
-    planner(player_cur_loc)
+    print_knowledge()
+
+    print("current location:",player_cur_loc,"  current direction:", player_cur_direction)
+    print_grid()
+
+    planner(player_cur_loc[0],player_cur_loc[1])
 
 
-     
+
